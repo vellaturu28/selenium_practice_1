f3307075ccc9ea19d1fa2f776026ffc3221c87b5
vellaturu28 <43941339+vellaturu28@users.noreply.github.com>
2018-10-11 10:13:39 -0700

    xpath file added

---
+++ introduction/src/introduction/ElementLocat_Chrome.java
@@ -0,0 +1,27 @@
+package introduction;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ElementLocat_Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Madhrui\\Downloads\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com");
+		System.out.println(driver.getTitle());
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Home decor");
+		driver.findElement(By.className("nav-input")).click();
+		driver.findElement(By.linkText("Bedroom")).click();
+		
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Books");
+		driver.findElement(By.className("nav-input")).click();
+		driver.findElement(By.linkText("Amazon Home")).click();
+		driver.close();
+
+	}
+
+}
---
+++ introduction/src/introduction/XpathCSS.java
@@ -0,0 +1,29 @@
+package introduction;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathCSS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Madhrui\\Downloads\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get(" http:\\www.facebook.com");
+		System.out.println(driver.getTitle());
+
+		// driver.findElement(By.xpath("//*[@id='email']")).sendKeys("xpath@gmail.com");
+		// driver.findElement(By.xpath("//*[@id='pass']")).sendKeys("password");
+		// driver.findElement(By.xpath("//*[@id='u_0_2']")).click();
+
+		// driver.findElement(By.id("u_0_2")).click();
+		// driver.close();
+
+		driver.findElement(By.cssSelector("#email")).sendKeys("csspath@gmail.com");
+		driver.findElement(By.cssSelector("#pass")).sendKeys("password");
+		driver.findElement(By.cssSelector("#u_0_2")).click();
+
+	}
+}
