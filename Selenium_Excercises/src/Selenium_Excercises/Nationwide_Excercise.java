37cab95ffaecb579a5d117603cc4b83646984991
vellaturu28 <43941339+vellaturu28@users.noreply.github.com>
2018-11-14 20:09:16 -0800

    rename

--- Selenium_Excercises/src/Selenium_Excercises/Nationwide_Excercise.java
+++ Selenium_Excercises/src/Selenium_Excercises/Nationwide_Excercise.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class Nationwide_excercise {
+public class Nationwide_Excercise {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
