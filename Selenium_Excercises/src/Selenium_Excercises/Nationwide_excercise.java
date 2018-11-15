4abe8d5cd7847d65497155b6bec6ed93c55952f2
vellaturu28 <43941339+vellaturu28@users.noreply.github.com>
2018-11-14 20:04:33 -0800

    nationwide file added

--- Selenium_Excercises/src/Selenium_Excercises/Nationwide_excercise.java
+++ Selenium_Excercises/src/Selenium_Excercises/Nationwide_excercise.java
@@ -6,21 +6,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class Tab_Excercises {
+public class Nationwide_excercise {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Madhrui\\Downloads\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.facebook.com");
+		driver.get("https://www.nationwide.com");
 		driver.manage().window().maximize();
-
+		driver.findElement(By.xpath("//*[@name='Car_Front_View-outline']")).click();
+		Thread.sleep(2000);
+		
 		Actions a = new Actions(driver);
-		a.moveToElement(driver.findElement(By.xpath("//input[@id='email']"))).click().sendKeys("email_id@gmail.com")
-				.sendKeys(Keys.TAB).sendKeys("password").sendKeys(Keys.ENTER).build().perform();
-
-		Thread.sleep(3000);
+		a.moveToElement(driver.findElement(By.xpath("//input[@id='homeowner']"))).click().sendKeys("94583")
+				.sendKeys(Keys.ENTER).build().perform();
+		
+		Thread.sleep(2000);
 		driver.close();
+
 	}
 
 }
